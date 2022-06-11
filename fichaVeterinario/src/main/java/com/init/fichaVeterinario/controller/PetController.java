9989bfa795eab21ad0575d61d7db0f3c83803055
Mogedano <eloisa.mogedano@gmail.com>
2022-06-11 13:22:28 +0200

    prueba 2

--- fichaVeterinario/src/main/java/com/init/fichaVeterinario/controller/PetController.java
+++ fichaVeterinario/src/main/java/com/init/fichaVeterinario/controller/PetController.java
@@ -26,7 +26,7 @@ public class PetController {
 	PetService petService;
 
 	// Listar mascotas
-	@GetMapping(value = "/listPet") // Para listar hacemos un Get prueba
+	@GetMapping(value = "/listPet") // Para listar hacemos un Get
 	public ResponseEntity<List<Pet>> listPet() {
 		try {
 			List<Pet> listPet = petService.listPet();
